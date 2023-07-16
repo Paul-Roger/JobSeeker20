60e384bd9e2ea661496b3e34b16f551c2051d2d1
Paul-Roger <105671930+Paul-Roger@users.noreply.github.com>
2023-07-16 22:39:16 +0200

    Update settings.py

--- PMJobSeeker/PMJobSeeker/settings.py
+++ PMJobSeeker/PMJobSeeker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j@wjjwf4vgx)g9qj^q-h2+%+(g6%-2b1wwu(p48axoly$ylk84'
+SECRET_KEY = 'django-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
